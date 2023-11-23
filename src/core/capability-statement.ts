75ad08e3bcc01a885806541c369ab8711d2908fa
biximilien <maxime.gauthier88@gmail.com>
2023-11-23 12:38:33 -0500

    feat: Update Capability Statement
    Update the Capability Statement text.

--- src/core/capability-statement.ts
+++ src/core/capability-statement.ts
@@ -1,24 +1,22 @@
 import { CapabilityStatement } from "@bonfhir/core/r5";
+import { narrative } from "@bonfhir/core/r5";
+
+// TODO: we'll probably want to keep this updated with respect to the implementation
 
 export const capabilityStatement: CapabilityStatement = {
   resourceType: "CapabilityStatement",
   id: "fhir-roast-terminology-server",
-  text: {
-    status: "extensions",
-    div: '<div xmlns="http://www.w3.org/1999/xhtml">\n      <h2>ACMETerminologyServiceCapabilityStatement</h2>\n      <div>\n        <p>Example capability statement for a Terminology Server. A server can support more fucntionality than defined here, but this is the minimum amount</p>\n\n      </div>\n      <table>\n        <tr>\n          <td>Mode</td>\n          <td>SERVER</td>\n        </tr>\n        <tr>\n          <td>Description</td>\n          <td/>\n        </tr>\n        <tr>\n          <td>Transaction</td>\n          <td/>\n        </tr>\n        <tr>\n          <td>System History</td>\n          <td/>\n        </tr>\n        <tr>\n          <td>System Search</td>\n          <td/>\n        </tr>\n      </table>\n      <table>\n        <tr>\n          <th>\n            <b>Resource Type</b>\n          </th>\n          <th>\n            <b>Profile</b>\n          </th>\n          <th>\n            <b title="GET a resource (read interaction)">Read</b>\n          </th>\n          <th>\n            <b title="GET all set of resources of the type (search interaction)">Search</b>\n          </th>\n          <th>\n            <b title="PUT a new resource version (update interaction)">Update</b>\n          </th>\n          <th>\n            <b title="POST a new resource (create interaction)">Create</b>\n          </th>\n        </tr>\n        <tr>\n          <td>CodeSystem</td>\n          <td>\n            <a href="http://hl7.org/fhir/StructureDefinition/CodeSystem">http://hl7.org/fhir/StructureDefinition/CodeSystem</a>\n          </td>\n          <td>y</td>\n          <td>y</td>\n          <td/>\n          <td/>\n        </tr>\n        <tr>\n          <td>ValueSet</td>\n          <td>\n            <a href="http://hl7.org/fhir/StructureDefinition/ValueSet">http://hl7.org/fhir/StructureDefinition/ValueSet</a>\n          </td>\n          <td>y</td>\n          <td>y</td>\n          <td/>\n          <td/>\n        </tr>\n        <tr>\n          <td>ConceptMap</td>\n          <td>\n            <a href="http://hl7.org/fhir/StructureDefinition/ConceptMap">http://hl7.org/fhir/StructureDefinition/ConceptMap</a>\n          </td>\n          <td>y</td>\n          <td>y</td>\n          <td/>\n          <td/>\n        </tr>\n      </table>\n    </div>',
-  },
   url: "http://hl7.org/fhir/CapabilityStatement/terminology-server",
   version: "5.0.0",
-  name: "ACMETerminologyServiceCapabilityStatement",
-  title: "ACME Terminology Service — Capability Statement",
+  name: "FHIRRoastTerminologyServiceCapabilityStatement",
+  title: "FHIR Roast Terminology Service — Capability Statement",
   status: "draft",
   experimental: true,
-  date: "2022-09-01",
-  description:
-    "Example capability statement for a Terminology Server. A server can support more functionality than defined here, but this is the minimum amount",
+  date: "2023-11-23",
+  description: "FHIR Roast is a bare-bone Terminology Server.",
   kind: "instance",
   implementation: {
-    description: "The ACME FHIR Terminology Server",
+    description: "The FHIR Roast Terminology Server",
   },
   fhirVersion: "5.0.0",
   format: ["json", "xml"],
@@ -262,3 +260,5 @@ export const capabilityStatement: CapabilityStatement = {
     },
   ],
 };
+
+capabilityStatement.text = narrative(capabilityStatement);
