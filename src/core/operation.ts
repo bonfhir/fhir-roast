91fc171a4c081740ec831f96d7658be4d2d430d5
biximilien <maxime.gauthier88@gmail.com>
2023-11-23 11:55:00 -0500

    feat: Update parameters to extend Operation
    Replace operation parameters to extends Operation.

--- src/core/operation.ts
+++ src/core/operation.ts
@@ -1,44 +1,149 @@
-import { Parameters } from "@bonfhir/core/r5";
+import { Operation } from "@bonfhir/core/r5";
 
-export type CodeSystemLookupOperation = {
-  code: Parameters;
-  system: Parameters;
-  version: Parameters;
-};
+export interface CodeSystemLookupOperation extends Operation {
+  parameters: {
+    resourceType: "Parameters";
+    parameter: [
+      {
+        name: "code";
+        valueCode: string;
+      },
+      {
+        name: "system";
+        valueUri: string;
+      },
+      {
+        name: "version";
+        valueString: string;
+      }
+    ];
+  };
+}
 
-export type CodeSystemValidateCodeOperation = {
-  code: Parameters;
-  system: Parameters;
-  version: Parameters;
-  display: Parameters;
-};
+export interface CodeSystemValidateCodeOperation extends Operation {
+  parameters: {
+    resourceType: "Parameters";
+    parameter: [
+      {
+        name: "code";
+        valueCode: string;
+      },
+      {
+        name: "system";
+        valueUri: string;
+      },
+      {
+        name: "version";
+        valueString: string;
+      },
+      {
+        name: "display";
+        valueString: string;
+      }
+    ];
+  };
+}
 
-export type CodeSystemSubsumesOperation = {
-  codeA: Parameters;
-  codeB: Parameters;
-  system: Parameters;
-  version: Parameters;
-};
+export interface CodeSystemSubsumesOperation extends Operation {
+  parameters: {
+    resourceType: "Parameters";
+    parameter: [
+      {
+        name: "codeA";
+        valueCode: string;
+      },
+      {
+        name: "codeB";
+        valueCode: string;
+      },
+      {
+        name: "system";
+        valueUri: string;
+      },
+      {
+        name: "version";
+        valueString: string;
+      }
+    ];
+  };
+}
 
-export type ValueSetExpandOperation = {
-  url: Parameters;
-  filter: Parameters;
-  offset: Parameters;
-  count: Parameters;
-};
+export interface ValueSetExpandOperation extends Operation {
+  parameters: {
+    resourceType: "Parameters";
+    parameter: [
+      {
+        name: "url";
+        valueUri: string;
+      },
+      {
+        name: "filter";
+        valueString: string;
+      },
+      {
+        name: "offset";
+        valueInteger: number;
+      },
+      {
+        name: "count";
+        valueInteger: number;
+      }
+    ];
+  };
+}
 
-export type ValueSetValidateCodeOperation = {
-  url: Parameters;
-  code: Parameters;
-  system: Parameters;
-  systemVersion: Parameters;
-  display: Parameters;
-};
+export interface ValueSetValidateCodeOperation extends Operation {
+  parameters: {
+    resourceType: "Parameters";
+    parameter: [
+      {
+        name: "url";
+        valueUri: string;
+      },
+      {
+        name: "code";
+        valueCode: string;
+      },
+      {
+        name: "system";
+        valueUri: string;
+      },
+      {
+        name: "systemVersion";
+        valueString: string;
+      },
+      {
+        name: "display";
+        valueString: string;
+      }
+    ];
+  };
+}
 
-export type ConceptMapTranslateOperation = {
-  url: Parameters;
-  sourceCode: Parameters;
-  system: Parameters;
-  targetCode: Parameters;
-  targetSystem: Parameters;
-};
+export interface ConceptMapTranslateOperation extends Operation {
+  parameters: {
+    resourceType: "Parameters";
+    parameter: [
+      {
+        name: "url";
+        valueUri: string;
+      },
+      {
+        name: "sourceCode";
+        valueCode: string;
+      },
+      {
+        name: "system";
+        valueUri: string;
+      },
+      {
+        name: "targetCode";
+        valueCode: string;
+      },
+      {
+        name: "targetSystem";
+        valueUri: string;
+      }
+    ];
+  };
+}
