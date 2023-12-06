ad165b5b92792ff8e7ab834a86513c8fedc6ebcc
biximilien <maxime.gauthier88@gmail.com>
2023-12-06 16:42:14 -0500

    feat: Scaffold browser index page
    Scaffold the browser index page with React components.

--- packages/browser/pages/index.tsx
+++ packages/browser/pages/index.tsx
@@ -1,36 +1,66 @@
-import * as styles from "./index.module.css";
+import { Coding } from "@bonfhir/core/r5";
+import styles from "./index.module.css";
 
-import { FC, useState } from "react";
+import { FC } from "react";
+
+const jsonUrl = (coding: Coding) => {
+  return `/CodeSystem/$lookup?system=${coding.system}&code=${coding.code}&format=json`;
+};
+
+const xmlUrl = (coding: Coding) => {
+  return `/CodeSystem/$lookup?system=${coding.system}&code=${coding.code}&format=xml`;
+};
+
+const SearchForm: FC = () => {
+  return (
+    <form>
+      <input type="text" placeholder="Query" />
+      <input type="submit" value="Search" />
+    </form>
+  );
+};
+
+const TableCoding: FC<{ codings: Array<Coding> }> = ({ codings }) => {
+  return (
+    <table>
+      <tr>
+        <th>Code</th>
+        <th>Display</th>
+        <th>System</th>
+        <th></th>
+      </tr>
+      {codings.map((coding) => (
+        <TableRowCoding coding={coding} />
+      ))}
+    </table>
+  );
+};
+
+const TableRowCoding: FC<{ coding: Coding }> = ({ coding }) => {
+  return (
+    <tr>
+      <td>{coding.code}</td>
+      <td>{coding.display}</td>
+      <td>{coding.system}</td>
+      <td>
+        <a href={jsonUrl(coding)} className={styles.link}>
+          JSON
+        </a>
+        <a href={xmlUrl(coding)} className={styles.link}>
+          XML
+        </a>
+      </td>
+    </tr>
+  );
+};
 
 export const IndexPage: FC = () => {
+  const codings: Array<Coding> = [];
   return (
-    <div>
-      <h1>FHIR Roast Terminology Service</h1>
-      <form>
-        <input type="text" placeholder="Query" />
-        <input type="submit" value="Search" />
-      </form>
-      <table>
-        <tr>
-          <th>Code</th>
-          <th>Display</th>
-          <th>System</th>
-          <th></th>
-        </tr>
-        <tr>
-          <td>123</td>
-          <td>Test</td>
-          <td>http://test.com</td>
-          <td>
-            <a href="#" className={styles.link}>
-              JSON
-            </a>
-            <a href="#" className={styles.link}>
-              XML
-            </a>
-          </td>
-        </tr>
-      </table>
+    <div className={styles.pageContainer}>
+      <h1 className="title">FHIR Roast Terminology Service</h1>
+      <SearchForm />
+      <TableCoding codings={codings} />
     </div>
   );
 };
